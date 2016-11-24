0635b66c1078f4dd55d987cce6bc184d0d6f2f7f
waliddif <walid.dif@gmail.com>
2016-11-24 22:16:02 +0000

    Committed from Rstudio by WD

--- R/f_setup.R
+++ R/f_setup.R
@@ -15,7 +15,8 @@
 f_setup <- function(){
   cv.packs <- c("zoo", "plm", "quantreg", "xlsx", "chron", "PerformanceAnalytics", "nlme",
                 "tseries", "forecast", "knitr", "pander", "rstudioapi", "rmarkdown", "psych",
-                "devtools", "shiny")
+                "devtools", "shiny",
+                "rpackagemock2")
   for (p in 1:length(cv.packs)) library(cv.packs[p], character.only=T)  # load installed packages
   print("Loaded the following packages:")
   print(cv.packs)
